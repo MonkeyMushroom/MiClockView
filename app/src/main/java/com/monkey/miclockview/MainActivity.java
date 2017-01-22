8633a11bedb4f08c3404fadc53284fc75b46e97e
Monkey <monkey@gmail.com>
2017-01-22 15:32:33 +0800

    完善注释

--- app/src/main/java/com/monkey/miclockview/MainActivity.java
+++ app/src/main/java/com/monkey/miclockview/MainActivity.java
@@ -9,6 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MiClockView clockView = (MiClockView) findViewById(R.id.clockView);
     }
 }
